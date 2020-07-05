84d112aae31237315c3151e40879bdef83afe0a0
Daniel Gordon <d_gordon1@icloud.com>
2020-07-05 16:54:16 +0200

    add data correctly

--- app.rb
+++ app.rb
@@ -1,14 +1,14 @@
 # frozen_string_literal: true
 
 require 'sinatra'
-require_relative 'mushroom_parser.rb'
+require_relative './services/mushroom_parser.rb'
 
 get '/' do
   erb :home
 end
 
 get '/shrooms' do
-  @result = MushRoomParser.new("./#{params[:file_name]}")
+  @result = MushRoomParser.new(file_name: params[:file_name])
                           .shrooms
 
   erb :index
