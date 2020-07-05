d62d9fee11f3de9315404d86a051eec8d4b5f15e
Daniel Gordon <d_gordon1@icloud.com>
2020-07-05 17:25:10 +0200

    add more specs

--- test/mushroom_parser_test.rb
+++ test/mushroom_parser_test.rb
@@ -3,6 +3,7 @@
 require_relative '../services/mushroom_parser.rb'
 
 require 'minitest/autorun'
+require 'pry'
 
 class MushRoomParserTest < Minitest::Test
   def test_it_raises_an_error_when_not_passed_named_argument
@@ -18,5 +19,22 @@ class MushRoomParserTest < Minitest::Test
   def test_it_returns_a_hash_of_shroom_data_for_valid_file_names
     service = MushRoomParser.new(file_name: 'agaricus-lepiota.data')
     assert_equal({ edible: 1264, non_edible: 1020 }, service.shrooms['brown'])
+  end
+
+  describe '#count_shrooms' do
+    def test_it_raises_a_no_method_error_when_invoked
+      service = MushRoomParser.new(file_name: 'file.data')
+
+      assert_raises(NoMethodError) { service.count_shrooms }
+    end
+
+    def test_it_returns_a_hash_of_data_when_invoked_using_send
+      service = MushRoomParser.new(file_name: 'agaricus-lepiota.data')
+      result = service.send(:count_shrooms)
+
+      assert_instance_of(Hash, result)
+      assert_equal(MushRoomParser::LETTERS_TO_COLOR.values.sort, result.keys.sort)
+      assert_equal({ edible: 400, non_edible: 672 }, result['yellow'])
+    end
   end
 end
